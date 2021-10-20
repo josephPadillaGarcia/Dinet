9908a04384aba7a8cd885469f7ae4578a3e520d0
josephpg <jlpadillagarcia19@gmail.com>
2021-10-20 02:26:01 -0500

    update new

--- public/js/app.js
+++ public/js/app.js
@@ -83,52 +83,10 @@ $('ul.navbar-nav li.dropdown').hover(function () {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
-let num_envios = document.getElementById('numero_envios');
-let num_unidades = document.getElementById('numero_unidades');
-let num_almacenes = document.getElementById('numero_almacenes');
 
-let cantidad = 256000;
-let cantidad_unidades = 125;
-let cantidad_alamcenes = 23;
-
-let tiempo=setInterval(()=>{
-    cantidad+=1;
-    num_envios.textContent=cantidad;
-
-    if(cantidad === 256156){
-        clearInterval(tiempo);
-    }
-
-}, 50);
-
-let tiempo_unidad=setInterval(()=>{
-    cantidad_unidades+=1;
-    num_unidades.textContent=cantidad_unidades;
-    if(cantidad_unidades === 352){
-        clearInterval(tiempo_unidad);
-    }
-
-}, 50);
-
-let tiempo_almacenes=setInterval(()=>{
-    cantidad_alamcenes+=1;
-    num_almacenes.textContent=cantidad_alamcenes ;
-    if(cantidad_alamcenes === 84){
-        clearInterval(tiempo_almacenes);
-    }
-
-}, 50);
-
-
-/*$(document).ready(function(){
-
-    $("#filtrar").click(function(){
-        $(".overlay").addClass("active");
-        $(".filter").addClass("active");
-        $('html, body').animate({
-            scrollTop: $("#section-filter").offset().top
-        }, 800);
+$(document).ready(function() {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 5000
     });
-
-
-});*/
+});
